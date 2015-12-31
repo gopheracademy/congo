35df1afcebc61e5fd007eec9f273d41d03985c05
Raphael Simon <simon.raphael@gmail.com>
2015-12-30 17:15:08 -0800

    Fix login links

--- ui/app/Login.js
+++ ui/app/Login.js
@@ -7,8 +7,8 @@ export default class Login extends React.Component {
       <div>
       <DocumentTitle title={`Sign In`} />
       <h3>Sign In With</h3>
-      <a className="btn btn-twitter" href="/auth/twitter"><i className="icon-twitter"></i> | Twitter </a>
-      <a className="btn btn-github" href="/auth/github"><i className="icon-github"></i> | GitHub </a>
+      <a className="btn btn-twitter" href="/api/auth/twitter"><i className="icon-twitter"></i> | Twitter </a>
+      <a className="btn btn-github" href="/api/auth/github"><i className="icon-github"></i> | GitHub </a>
       </div>
     );
   }
