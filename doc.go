296cf208e0cbf0fb2266df9e49db2cb70e60d4b1
Brian Ketelsen <bketelsen@gmail.com>
2016-07-21 10:25:42 -0400

    Update doc.go

--- doc.go
+++ doc.go
@@ -1,10 +1,4 @@
 //go:generate go run assets_gen.go assets.go
 
-// Gopherpen is a project template that will let you easily get started with GopherJS
-// for building a web app. It includes some simple HTML, CSS, and Go code for the frontend.
-// Make some changes, and refresh in browser to see results. When there are errors in your
-// frontend Go code, they will show up in the browser console.
-//
-// Once you're done making changes, you can easily create a fully self-contained static
-// production binary.
+
 package main
